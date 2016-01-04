6823f3f30d4ec061400e501e96ed605e7e2a7a32
Zein Fakhreddine <Zein.Fakhreddine@gmail.com>
2016-01-05 00:30:15 +0300

    changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var http = require('http');
 
 //Lets define a port we want to listen to
-const PORT=8080; 
+var PORT=Number(process.env.PORT || 3000); 
 
 //We need a function which handles requests and send response
 function handleRequest(request, response){
